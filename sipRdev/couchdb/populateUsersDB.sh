75df2fb48b69fea9972a91f9ddf02df06d2102c0
dizzy <george@ezuce.com>
2015-11-10 13:44:01 +0200

    Add docker dev env (couchdb + consul), script to populate couchdb for load testing

--- sipRdev/couchdb/populateUsersDB.sh
+++ sipRdev/couchdb/populateUsersDB.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# test user
+curl -H 'Content-Type: application/json' -X POST http://127.0.0.1:5984/sipr -d '{"userName": "200", "sipPassword" : "12345"}'
+
+# load users
 for i in $(seq 10000 20000)
 do
  curl -H 'Content-Type: application/json' -X POST http://127.0.0.1:5984/sipr -d '{"userName": "$i", "sipPassword" : "1234"}'
